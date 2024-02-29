539ed5aafdc1f5c8968be72e4a5405a4f219e6c8
ebadihamed <ebadihamed18@gmail.com>
2024-02-29 15:42:18 +0200

    refactored code

--- routes.py
+++ routes.py
@@ -9,86 +9,89 @@ def index():
     blogs = functions.get_blogs()
     return render_template("index.html", blogs=blogs)
 
-@app.route("/sign_in")
-def sign_in():
-    return render_template("sign_in.html")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    if request.method == "POST":
+        # Getting the credentials 
+        username = request.form["uname"]
+        password = request.form["password"]
 
-@app.route("/sign_up")
-def sign_up():
-    return render_template("sign_up.html")
+        # Getting the credentials from the database
+        user = functions.get_users(username)
 
-@app.route("/log_in", methods=["POST"])
-def log_in():
-    # Getting the credentials 
-    username = request.form["uname"]
-    password = request.form["password"]
+        # If user is not in the database, then raises alert
+        if not user:
+            print("user not found")
 
-    # Getting the credentials from the database
-    user = functions.get_users(username)
-
-    # If user is not in the database, then raises alert
-    if not user:
-        print("user not found")
-
-    # If user exists then checks the password and Logs in
-    else:
-        user_id = user[0]
-        result = functions.get_credentials(user_id)
-        password_hash = result[0].password_hash
-
-        if check_password_hash(password_hash, password):
-            session["username"] = username
-            session["user_id"] = user_id
-            print("logged in")
-
+        # If user exists then checks the password and Logs in
         else:
-            print("invalid password")
+            user_id = user[0]
+            result = functions.get_credentials(user_id)
+            password_hash = result[0].password_hash
 
-    return redirect("/")
+            if check_password_hash(password_hash, password):
+                session["username"] = username
+                session["user_id"] = user_id
+                print("logged in")
 
-@app.route("/new_account", methods=["POST"])
-def new_account():
-    # Getting the credentials
-    firstname = request.form['fname']
-    username = request.form['uname']
-    password = request.form['password']
-    password_hash = generate_password_hash(password)
+            else:
+                print("invalid password")
 
-    # Checking if the username already exists in the database
-    user = functions.get_users(username)
+        return redirect("/")
 
-    # If such user exists in the database, then alert the user
-    if user:
-        pass
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    if request.method == "POST":
+        # Getting the credentials
+        firstname = request.form['fname']
+        username = request.form['uname']
+        password = request.form['password']
+        password_1 = request.form['password_1']
+        password_hash = generate_password_hash(password)
 
-    # If user is not in the database then we can create new one
-    if not user:
-        functions.new_user(username, firstname, password_hash)
+        # Checking if the username already exists in the database
+        user = functions.get_users(username)
 
-    return redirect("/sign_in")
+        # If such user exists in the database, then alert the user
+        if user:
+            pass
+
+        # If user is not in the database then we can create new one
+        if not user:
+            if password == password_1:
+                functions.new_user(username, firstname, password_hash)
+            else:
+                print("incorrect password")
+        return redirect("/")
 
 @app.route("/log_out")
 def log_out():
     del session["username"]
     return redirect("/")
 
-@app.route("/new_blog")
+@app.route("/new_blog", methods=["GET", "POST"])
 def new_blog():
-    return render_template("new.html")
+    if request.method == "GET":
+        return render_template("new.html")
+    
+    if request.method == "POST":
+        # Get the content from POST and user_id from the database
+        username = session["username"]
+        content = request.form['content']
+        result = functions.get_users(username)
+        print(result)
+        user_id = result[0]
 
-@app.route("/create", methods=["POST"])
-def create():
-    # Get the content from POST and user_id from the database
-    username = session["username"]
-    content = request.form['content']
-    result = functions.get_users(username)
-    print(result)
-    user_id = result[0]
+        # Send the content and additional information to the database to be saved
+        functions.new_blog(content, user_id)
 
-    # Send the content and additional information to the database to be saved
-    functions.new_blog(content, user_id)
-
-    return redirect("/")
+        return redirect("/")
 
 @app.route("/like", methods=["POST"])
 def like():
@@ -108,12 +111,11 @@ def like():
 def edit():
     blog_id = request.form["blog_id"]
     result = functions.get_content(blog_id)
-    print(result)
     content = result[0]
     return render_template("edit.html", content=content, id=blog_id)
 
-@app.route("/edit_cancel", methods=["POST"])
-def edit_cancel():
+@app.route("/cancel", methods=["POST"])
+def cancel():
     return redirect("/")
 
 @app.route("/edit_save", methods=["POST"])
