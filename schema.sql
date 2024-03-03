d49f6cc0cd603c72f1549fba40560e1297289521
ebadihamed <ebadihamed18@gmail.com>
2024-03-03 18:33:39 +0200

    database updated

--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
-Create TABLE admins (
+-- Create TABLE admins
+CREATE TABLE admins (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Create TABLE users
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
@@ -12,29 +14,40 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Create TABLE credentials
 CREATE TABLE credentials (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
     password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE blogs(
+-- Create TABLE blogs
+CREATE TABLE blogs (
     id SERIAL PRIMARY KEY,
-    content text,
-    user_id INTEGER REFERENCES users,
+    content TEXT,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE blog_likes(
+-- Create TABLE blog_likes
+CREATE TABLE blog_likes (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users,
-    blog_id INTEGER REFERENCES blogs,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    blog_id INTEGER REFERENCES blogs ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users (username, name, created_at) VALUES ("hamedebadi", "hamed", NOW());
-INSERT INTO credentials (user_id, password_hash, created_at) VALUES (1, 12345, NOW());
-INSERT INTO blogs (content, user_id, created_at) VALUES ("This is the first blog created by me to and you can create your own by clicking on creating button!", 1, NOW());
-INSERT INTO blogs (content, user_id, created_at) VALUES ("It's crazy that we have to study math for computer science, I mean who even knew that fact, right?", 1, NOW());
-INSERT INTO blogs (content, user_id, created_at) VALUES ("I wish every course on computer science program was this interesting and easy!!!", 1, NOW());
+-- Insert sample data for users
+INSERT INTO users (username, name, created_at) 
+VALUES ('hamedebadi', 'hamed', NOW());
+
+-- Insert sample data for credentials
+INSERT INTO credentials (user_id, password_hash, created_at) 
+VALUES (1, 'hashed_password', NOW());
+
+-- Insert sample data for blogs
+INSERT INTO blogs (content, user_id, created_at) 
+VALUES ('This is the first blog created by me to and you can create your own by clicking on creating button!', 1, NOW()),
+       ('It''s crazy that we have to study math for computer science, I mean who even knew that fact, right?', 1, NOW()),
+       ('I wish every course on computer science program was this interesting and easy!!!', 1, NOW());
